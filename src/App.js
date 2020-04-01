8d688ede938c366e1532e8682941f1def0bd9c10
netoanttunes <netoanttunes@gmail.com>
2020-04-01 00:16:04 -0300

    Front-end Pronto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+
 import './App.css';
 
-function App() {
+export default function App() {
+  const [planet, setPlanet] = useState ([]);
+  const [id, setId] = useState (1);
+  const [films, setFilms] = useState ([])
+
+    
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`https://swapi.co/api/planets/${id}`);
+      const data = await response.json();
+      setPlanet(data);
+      
+      }
+    fetchData();
+  }, [id]);
+
+  useEffect(() => {
+    async function filmsNumber() {
+      const response = await fetch(`https://swapi.co/api/planets/${id}`);
+      const { films } = await response.json();
+      setFilms(films);
+      }
+    filmsNumber();
+  }, [id]);
+
+  function handleNext(min, max){
+     min = Math.ceil(min);
+     max = Math.floor(max);
+    return setId( Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+       <strong><h1>Start Wars Game, What is the Planet?</h1></strong> 
       </header>
+
+      <form >
+        <ul>
+           <li className="Planeta"><h1>{planet.name}</h1></li>
+           <li><h2>Population: {planet.population}</h2></li>
+           <li><h2>Climate: {planet.climate}</h2></li>
+           <li><h2>Terrain: {planet.terrain}</h2></li>
+           <li><h2>Featured in {films.length} Films</h2></li>
+                
+        </ul> 
+       </form>
+      
+       <button className="button" onClick={() => handleNext(1,62)}>NEXT</button>
+
     </div>
   );
 }
 
-export default App;
+
