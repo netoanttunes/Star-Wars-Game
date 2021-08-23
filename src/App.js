d81489b673a4b6e92b59dd9255bea88a12a16fe6
João Antunes <joao.antunes@efabrika.com.br>
2021-08-23 13:11:37 -0300

    Atualização no useEffect

--- src/App.js
+++ src/App.js
@@ -6,25 +6,16 @@ export default function App() {
   const [planet, setPlanet] = useState ([]);
   const [id, setId] = useState (1);
   const [films, setFilms] = useState ([])
-
     
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(`https://swapi.co/api/planets/${id}`);
+      const response = await fetch(`https://swapi.dev/api/planets/${id}`);
       const data = await response.json();
+      const { films } = data;
       setPlanet(data);
-      
-      }
-    fetchData();
-  }, [id]);
-
-  useEffect(() => {
-    async function filmsNumber() {
-      const response = await fetch(`https://swapi.co/api/planets/${id}`);
-      const { films } = await response.json();
       setFilms(films);
       }
-    filmsNumber();
+    fetchData();
   }, [id]);
 
   function handleNext(min, max){
@@ -35,19 +26,21 @@ export default function App() {
  
   return (
     <div className="App">
+
+      <h1 className="titulo">Qual é o Planeta?</h1>
      
       <form>
         <ul>
            <li className="Planeta"><h1>{planet.name}</h1></li>
-           <li><h2>Population: {planet.population}</h2></li>
-           <li><h2>Climate: {planet.climate}</h2></li>
-           <li><h2>Terrain: {planet.terrain}</h2></li>
-           <li><h2>Featured in {films.length} Films</h2></li>
+           <li><h2>População: {planet.population}</h2></li>
+           <li><h2>Clima: {planet.climate}</h2></li>
+           <li><h2>Terreno: {planet.terrain}</h2></li>
+           <li><h2>Apareceu em {films.length} Filmes</h2></li>
                 
         </ul> 
        </form>
       
-       <button className="button" onClick={() => handleNext(1,62)}>NEXT</button>
+       <button className="button" onClick={() => handleNext(1,62)}>Vai</button>
 
     </div>
   );
